64f812c555a6fa963f8fdc077a4043c2b2bd8590
Galon123 <leejohnsanthosh12345@gmail.com>
2026-01-04 18:09:33 +0530

    Added Utils.py

---
+++ backend/Generations/utils.py
@@ -0,0 +1,10 @@
+
+def generate_timetable(teachers,room_list,time_list):
+
+    #Condition 1: On a Specific day,a teacher can only occupy one class at a time
+    #Condition 2: All Periods must be filled
+    #Condition 3: A teacher can take n classes per week, the total of all should be (no of rooms * no of days * no of time slots)
+    #Condition 4: There should not be more than 1 class of the same teacher on the same room
+    #Condition 5: There should not be a free period in the morning
+    #Condition 6: 12pm is strictly for Lunch
+    pass
