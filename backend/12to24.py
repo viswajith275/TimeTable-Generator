26380782c5a03647b8e4522757b17dbc5b5db87b
asifali411 <asifalipm411@gmail.com>
2026-01-08 22:45:35 +0530

    removed a bunch of unneccesary files

--- backend/12to24.py
+++
@@ -1,9 +0,0 @@
-time=(input("enter time as hr.min AM/PM:"))
-
-def convert(time):
-    t = time.split() # ["hr.min" , "AM/PM"]
-    if t[1] == "PM":
-        return (int(t[0].split(".")[0]) + 12, int(t[0].split(".")[1]))
-    return (int(t[0].split(".")[0]), int(t[0].split(".")[1]))
-
-print(convert(time))
