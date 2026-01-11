e44e7751a20b90bb907ef99ae0848c76cc7d3e05
viswajith275 <viswajithmangottil@gmail.com>
2026-01-11 09:38:09 +0530

    changed one dependancy

--- backend/oauth.py
+++ backend/oauth.py
@@ -1,5 +1,5 @@
 import jwt
-from datetime import datetime,timedelta, timezone
+from datetime import datetime,timedelta
 from backend.config import SECRET_KEY, ALGORITHM
 from pwdlib import PasswordHash
 from typing import Annotated
