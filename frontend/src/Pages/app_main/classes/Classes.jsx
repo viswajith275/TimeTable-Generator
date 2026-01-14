7f7b0dc6323c21886e863b2062d7f2909da31e20
geo-777 <geo.binu.25@gmail.com>
2026-01-14 10:41:51 +0530

    added Loader in classes

--- frontend/src/Pages/app_main/classes/Classes.jsx
+++ frontend/src/Pages/app_main/classes/Classes.jsx
@@ -6,6 +6,7 @@ import { useEffect, useState } from "react";
 import ClassDetailsPopup from "./Components/ClassDetailsPopup";
 import ClassItem from "./Components/ClassItem";
 import Filter from "../Components/filter/Filter";
+import ErrorLoadingStatesClasses from "./Components/ErrorLoadingStatesClasses";
 import axios from "axios";
 
 const Classes = () => {
@@ -13,10 +14,17 @@ const Classes = () => {
   const [popupShow, setPopupShow] = useState(false);
   const [classes, setClasses] = useState({});
   const [isClassesLoading, setIsClassesLoading] = useState(true);
+  const [classesLoadingErrorStates, setClassesLoadingErrorStates] =
+    useState(null);
 
   //fetches all the classes on mounting of component
   useEffect(() => {
     const fetchAllClasses = async () => {
+      // Ensure the loader is visible for at least MIN_TIME.
+      // If the API responds faster, we wait the remaining time.
+      // If it takes longer, we stop loading immediately (no extra delay). Math.max is used to prevent that (in case of -ve delay, make it 0)
+      // this prevents that flickering problem.. so kind of a smooth transition
+      const start = Date.now();
       try {
         const { data } = await axios.get("/api/classes"); // instead of response.data  destructure that object
 
@@ -24,7 +32,11 @@ const Classes = () => {
       } catch (err) {
         console.log(err);
       } finally {
-        setIsClassesLoading(false);
+        const MIN_TIME = 500; //in ms
+        const elapsed = Date.now() - start;
+        setTimeout(() => {
+          setIsClassesLoading(false);
+        }, Math.max(MIN_TIME - elapsed, 0)); // to avoid negative numbers here..
       }
     };
 
@@ -74,15 +86,22 @@ const Classes = () => {
         <div className={styles.gridClasses}>
           {/* have used roomName instead of className to avoid confusion
             as classNaame is a pre-defined keyword in jsx */}
-          {Object.values(classes).map((value) => {
-            return (
-              <ClassItem
-                key={value.id}
-                roomName={value.c_name}
-                roomNumber={value.r_name}
-              />
-            );
-          })}
+
+          {isClassesLoading ? (
+            <ErrorLoadingStatesClasses state={"loading"} />
+          ) : (
+            Object.values(classes).map((value) => {
+              return (
+                <ClassItem
+                  key={value.id}
+                  roomName={value.c_name}
+                  roomNumber={value.r_name}
+                />
+              );
+            })
+          )}
+
+          {classes.length == 0 ? console.log("ho") : ""}
         </div>
       </div>
     </div>
--- frontend/src/Pages/app_main/classes/Components/ErrorLoadingStatesClasses.jsx
+++ frontend/src/Pages/app_main/classes/Components/ErrorLoadingStatesClasses.jsx
@@ -1,7 +1,18 @@
 import React from "react";
-
+import Loader from "../../Components/loader/Loader";
+import styles from "./ErrorLoadingStatesClasses.module.css";
 const ErrorLoadingStatesClasses = ({ state }) => {
-  return <div></div>;
+  return (
+    <div className={styles.containerMain}>
+      {state === "loading" ? (
+        <Loader></Loader>
+      ) : (
+        <div>
+          <p>Error While Loading</p>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default ErrorLoadingStatesClasses;
